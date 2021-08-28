abececa38de66d848ed74c5fa4bd86a0a78b550e
PrinceNeZha <384751024@qq.com>
2021-08-28 10:38:29 +0800

    fix LinearSolver bugs

--- LinearSolver.py
+++ LinearSolver.py
@@ -3,9 +3,7 @@ import numpy as np
 
 ti.init(arch=ti.gpu)
 
-iterationNum = 100
-epsilon      = 1e-6
-n            = 100
+n  = 7
 
 x  = ti.field(dtype = ti.f32,shape=n)
 xn = ti.field(dtype = ti.f32,shape=n)
@@ -13,8 +11,13 @@ b  = ti.field(dtype = ti.f32,shape=n)
 r  = ti.field(dtype = ti.f32,shape=n)
 A  = ti.field(dtype = ti.f32,shape=(n,n))
 
+# preconditioner
+M  = ti.field(dtype = ti.f32,shape=(n,n))
+
 rn = ti.field(dtype = ti.f32,shape=n)
 p  = ti.field(dtype = ti.f32,shape=n)
+#temporary vector for intermedate variables
+t  = ti.field(dtype = ti.f32,shape=n)
 
 # r = b - Ax or r = xn-x
 @ti.kernel
@@ -23,7 +26,6 @@ def Residual():
         r[i] = b[i]
         for j in range(n):
             r[i]-=A[i,j]*x[j]
-        p[i] = r[i]
 
 @ti.kernel
 def Jacobi():
@@ -36,108 +38,73 @@ def Jacobi():
     for i in range(n):
         x[i] = xn[i]
 
+#NOTE : taichi Gauss-Seidel can be buggy because of kernel's 
+# auto parallel. Use red-black Gauss-Seidel instead. or use a 
+# single loop to block parallel.
 @ti.kernel
 def GaussSeidel():
-    for i in range(n):
-        bi = b[i]
-        for j in range(n):
-            if i==j:continue
-            bi-=A[i,j]*xn[j]
-        xn[i] = bi/A[i,i]
-    for i in range(n):
-        x[i] = xn[i]
-
-@ti.kernel
-def SuccessiveOverRelax(w:ti.f32):
-    for i in range(n):
-        bi = b[i]
-        for j in range(n):
-            if i==j:continue
-            bi-=A[i,j]*xn[j]
-        xn[i] = bi/A[i,i]
-    for i in range(n):
-        x[i] = xn[i]*w+(1-w)*x[i]
-
-@ti.kernel
-def GradientDescent():
-    alpha = 0.0
-    denominator = 0.0
-    for i in range(n):
-        alpha += r[i]*r[i]
-        Ari = 0.0
-        for j in range(n):
-            Ari += A[i,j]*r[j]
-        denominator += r[i]*Ari 
-    alpha/=denominator
-    for i in range(n):
-        x[i] += alpha*r[i]
+    for _ in range(1):
+        for i in range(n):
+            bi = b[i]
+            for j in range(n):
+                if i==j:continue
+                bi-=A[i,j]*x[j]
+            x[i] = bi/A[i,i]
 
 @ti.kernel
 def ConjugateGradient():
+    # calculate t = A*p
+    for i in range(n):
+        t[i] = 0.0
+        for j in range(n):
+            t[i] += A[i,j]*p[j]
     # calculate alpha
     alphaNumerator   = 0.0
     alphaDenominator = 0.0
     for i in range(n):
-        alphaNumerator += r[i]*r[i]
-        Ari = 0.0
-        for j in range(n):
-            Ari += A[i,j]*p[j]
-        alphaDenominator += Ari * p[i] 
+        alphaNumerator   += r[i]*r[i]
+        alphaDenominator += t[i]*p[i] 
     alpha = alphaNumerator/alphaDenominator
-    # update x and compute new residual(rn = r - alpha*Ap or rn == b - Ax)
-    for i in range(n):
-        x[i] += alpha*r[i]
-        Api = 0.0
-        for j in range(n):
-            Api += A[i,j]*p[j]
-        rn[i] = r[i] - alpha*Api 
-    #compute beta
+    # update x and compute new residual(rn = r - alpha*Ap or rn = b - Ax) and beta
     betaNumerator = 0.0 
-    betaDenominator = 0.0
+    betaDenominator = alphaNumerator
     for i in range(n):
-        betaNumerator += rn[i]*rn[i]
-        betaDenominator += r[i] *r[i]
+        x[i] = x[i] + alpha*p[i]
+        r[i] = r[i] - alpha*t[i]
+        betaNumerator += r[i]*r[i]
     beta = betaNumerator/betaDenominator
     #update p
     for i in range(n):
-        p[i] = rn[i] + beta *p[i]
-
-# A = [  2.5,-1,0,0,...., -1
-#         -1,2.5,-1,0,0,...
-#         0,-1,2.5,-1,0,0,...
-#     ]
+        p[i] = r[i] + beta *p[i]
+        
 @ti.kernel
 def Initialize():
     for i in range(n):
-        x[i] = 0
-        b[i] = 0
-    b[0] = 1
-    for i in range(n):
-        for j in range(n):
-            A[i,j] = 0
-    for i in range(n):
+        x[i] = 0.0
         A[i,i] = 2.5
-        if i+1<n: A[i,i+1]=-1 
-        if i-1>=0:A[i,i-1]=-1
+        if i+1<=n-1: A[i,i+1] = -1
+        if i-1>=0:   A[i,i-1] = -1
+        M[i,i] = A[i,i]
     A[0,n-1]=-1
     A[n-1,0]=-1
+    for i in range(n):
+        b[i] = 0.0
+    b[0] = 1
+    for i in range(n):
+        r[i] = b[i]
+        for j in range(n):
+            r[i]-=A[i,j]*x[j]
+        p[i] = r[i]
 
-
-def LinearSolver():
+#有些矩阵迭代会发散
+def LinearSolver(iterateMethod = Jacobi,iterationNum=100,epsilon=1e-6):
     Initialize()
-    Residual()
-    for _ in range(iterationNum):
-        # Jacobi()
-        # GaussSeidel()
-        # SuccessiveOverRelax(1.2)
-        # GradientDescent()
-        ConjugateGradient()
+    _ = 0
+    while _<iterationNum and np.max(np.abs(r.to_numpy()))>epsilon:
+        iterateMethod()
         Residual()
-        if np.max(np.abs(r.to_numpy()))<epsilon:
-            print("iteration count = ",_)
-            # print("x = ",x)
-            print("r = ",r)
-            return
-    print("No Convergence !")
+        _+=1
+    if _<iterationNum: print("iteration count =",_+1,"\n x =",x)
+    else:              print("No Convergence ! x = ",x)
 
-LinearSolver()
+LinearSolver(iterateMethod = ConjugateGradient)
