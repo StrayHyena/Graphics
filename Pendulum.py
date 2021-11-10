05b5b33422845568258131f5fdfafa4b02dbe071
PrinceNeZha <384751024@qq.com>
2021-11-10 12:39:42 +0800

    polish code

--- Pendulum.py
+++ Pendulum.py
@@ -17,31 +17,31 @@ def Initialize():
         indices[2*i] = i
         indices[2*i+1] = i+1
     p[0] = ti.Vector([0.5,0.5]) # base position
-    ys[0] = 3.1416*1.5/2
-    ys[1] = 0
+    ys[0] = 3.1416*6/8
+    ys[1] = 3.1416*4/8
     ys[2] = ys[3] = 0
 
 @ti.func
-def f(y):
-    b0 = -(m[0]+m[1])*g*ti.sin(y[0])-m[1]*l[1]*ti.sin(y[0]-y[1])*y[3]*y[3]
-    b1 = l[0]*ti.sin(y[0]-y[1])*y[2]*y[2]-g*ti.sin(y[1])
+def f(theta0,theta1,omega0,omega1):
+    b0 = -(m[0]+m[1])*g*ti.sin(theta0)-m[1]*l[1]*ti.sin(theta0-theta1)*omega1*omega1
+    b1 = l[0]*ti.sin(theta0-theta1)*omega0*omega0-g*ti.sin(theta1)
     b = ti.Vector([b0,b1])
 
     a00 = (m[0]+m[1])*l[0]
-    a01 = m[1]*l[1]*ti.cos(y[0]-y[1])
-    a10 = l[0]*ti.cos(y[0]-y[1])
+    a01 = m[1]*l[1]*ti.cos(theta0-theta1)
+    a10 = l[0]*ti.cos(theta0-theta1)
     a11 = l[1]
     A_inv = ti.Matrix([[a11,-a01],[-a10,a00]])/(a00*a11-a01*a10)
     acc = A_inv@b
-    return ti.Vector([ y[2] , y[3] , acc[0], acc[1] ]) #[ d(theta1)/dt, d(theta2)/dt, d(omega1)/dt, d(omega2)/dt]  
+    return ti.Vector([ omega0 , omega1 , acc[0], acc[1] ]) #[ d(theta1)/dt, d(theta2)/dt, d(omega1)/dt, d(omega2)/dt]  
 
 # 4th order Runge-Kutta to solve ODEs
 @ti.kernel
 def RK4():
-    k1 = f(ti.Vector([ys[0],ys[1],ys[2],ys[3]]))
-    k2 = f(ti.Vector([ys[0]+dt*k1[0]/2,ys[1]+dt*k1[1]/2,ys[2]+dt*k1[2]/2,ys[3]+dt*k1[3]/2]))
-    k3 = f(ti.Vector([ys[0]+dt*k2[0]/2,ys[1]+dt*k2[1]/2,ys[2]+dt*k2[2]/2,ys[3]+dt*k2[3]/2]))
-    k4 = f(ti.Vector([ys[0]+dt*k3[0],ys[1]+dt*k3[1],ys[2]+dt*k3[2],ys[3]+dt*k3[3]]))
+    k1 = f(ys[0],            ys[1],            ys[2],            ys[3])
+    k2 = f(ys[0]+dt*k1[0]/2, ys[1]+dt*k1[1]/2, ys[2]+dt*k1[2]/2, ys[3]+dt*k1[3]/2)
+    k3 = f(ys[0]+dt*k2[0]/2, ys[1]+dt*k2[1]/2, ys[2]+dt*k2[2]/2, ys[3]+dt*k2[3]/2)
+    k4 = f(ys[0]+dt*k3[0],   ys[1]+dt*k3[1],   ys[2]+dt*k3[2],   ys[3]+dt*k3[3])
     for i in ti.static(range(4)):
         ys[i]=ys[i]+(dt/6)*(k1[i]+2*k2[i]+2*k3[i]+k4[i])
     # compute position using theta （从广义坐标得到笛卡尔坐标）
