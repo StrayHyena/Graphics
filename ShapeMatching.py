b2097cb4a3bedba40c559909d905d45a3382e6a3
PrinceNeZha <384751024@qq.com>
2022-07-11 20:35:18 +0800

    inverse mass

--- ShapeMatching.py
+++ ShapeMatching.py
@@ -1,14 +1,16 @@
 import taichi as ti
 ti.init(ti.cpu)
 
-n = 10
-a,r = 0.3,0.005 # scale, radius
+n = 20
+a,r = 0.3,0.01 # scale, radius
 dt = 0.001
+eps = 1e-12
 
 x   = ti.Vector.field(2,ti.f32,n)
 xr  = ti.Vector.field(2,ti.f32,n)  # x rest 
 xn  = ti.Vector.field(2,ti.f32,n)  # x next
 v   = ti.Vector.field(2,ti.f32,n)
+inv_m   = ti.field(ti.f32,n)
 R   = ti.Matrix.field(2,2,ti.f32,())
 # a rotation with radian w, Rotation(w)
 # the deformation matrix is A,the raotion part of it is R_
@@ -23,30 +25,34 @@ def ExtractRotation(A,R,maxIter):
         a1 = ti.Vector([A[0,1],A[1,1]])
         r0 = ti.Vector([R[0,0],R[1,0]])
         r1 = ti.Vector([R[0,1],R[1,1]])
-        w = -(a0.cross(r0) + a1.cross(r1))/(ti.abs(a0.dot(r0) + a1.dot(r1))+1e-9)
+        w = -(a0.cross(r0) + a1.cross(r1))/(ti.abs(a0.dot(r0) + a1.dot(r1))+eps)
         R = ti.Matrix([[ti.cos(w),-ti.sin(w)],[ti.sin(w),ti.cos(w)]]) @ R
     return R
 
 @ti.kernel
 def Initialize():
     for i in x:
-        v[i] = [0.0,0.0]
         xr[i] = [ti.random()*a+0.35,ti.random()*a+0.35]
         x[i]  = xr[i]
         xn[i] = xr[i]
+        inv_m[i] = 1.0
+        v[i] = [0.0,0.0]
     R[None] = ti.Matrix.identity(ti.f32,2)
 
 @ti.kernel
 def Substep():
     xrc  = ti.Vector([0.0,0.0])
     xnc  = ti.Vector([0.0,0.0])
+    totalm = 0.0
     for i in xn:
-        v[i]  += ti.Vector([0.0,-2.0])*dt
+        v[i]  += ti.Vector([0.0,-2.0])*inv_m[i]*dt
         xn[i] = x[i] + v[i]*dt
-        xrc += xr[i]
-        xnc += xn[i]
-    xrc /=n
-    xnc /=n
+        m = 1.0/(inv_m[i]+eps)
+        xrc += xr[i]*m
+        xnc += xn[i]*m
+        totalm += m
+    xrc /= totalm
+    xnc /= totalm
     A_pq = ti.Matrix.zero(ti.f32,2,2)
     for i in x:
         A_pq += (xn[i]-xnc)@(xr[i]-xrc).transpose()
