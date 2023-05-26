7e76c8ba84a67d0667797d94d449a771778ae9ee
PrinceNeZha <384751024@qq.com>
2023-05-26 08:50:51 +0800

    BVH bin partition

--- Rendering/BVH2D.py
+++ Rendering/BVH2D.py
@@ -7,10 +7,11 @@ EPS     = 1e-8
 vec2  = ti.types.vector(2,ti.f32)
 vec3  = ti.types.vector(3,ti.f32)
 vec2i = ti.types.vector(2,ti.i32)
-ti.init(arch=ti.cpu)
+ti.init(arch=ti.cpu,cpu_max_num_threads=1)
 
 n = 20 # how many edges
 leaf_prim_count = 2
+bin_cnt = 8
 
 aabb     = ti.types.struct(bmin = vec2,bmax = vec2)
 bvh_node = ti.types.struct(box = aabb, li = ti.u32,ri = ti.u32, start = ti.u32, end = ti.u32)  # end < start indicates a non-leaf node
@@ -27,8 +28,8 @@ boxs_pts = [ ti.Vector.field(2, ti.f32,8) for _ in range(2*n) ]
 boxs_idx = ti.field(ti.i32,8)
 
 def AABBGrow(this,other):
-    this.bmax = vec2(max(this.bmax[0],other.bmax[0]),max(this.bmax[1],other.bmax[1]))
     this.bmin = vec2(min(this.bmin[0],other.bmin[0]),min(this.bmin[1],other.bmin[1]))
+    this.bmax = vec2(max(this.bmax[0],other.bmax[0]),max(this.bmax[1],other.bmax[1]))
 def AABBCenter(this):
     return (this.bmin+this.bmax)/2.0
 
@@ -38,40 +39,53 @@ def AABBCenter(this):
 #            .               ..                  
 #           right_child -> node_idx + 1 + left_node_count
 # [start,end]    NOT [start,end)   return how many nodes this tree have
-def BVHBuildSAH(node_idx,start,end):
-    assert(node_idx<2*n)
+
+def BuildBVH(node_idx,start,end):
     this_node = nodes[node_idx]
-    this_node.box = aabb( vec2(FLT_MAX,FLT_MAX) , -vec2(FLT_MAX,FLT_MAX))
-    
     split_axis,split_pos,split_cost = -1,-1,FLT_MAX
     left_count , right_count = 0,0
 
-    for i in range(start,end+1):
-        curr_split_pos = AABBCenter(aabbs[eidx[i]])
-        AABBGrow(this_node.box, aabbs[eidx[i]])
-        for j in range(2):
-            l_aabb = aabb(vec2(FLT_MAX,FLT_MAX),-vec2(FLT_MAX,FLT_MAX))
-            r_aabb = aabb(vec2(FLT_MAX,FLT_MAX),-vec2(FLT_MAX,FLT_MAX))
-            lc,rc  = 0,0
-            for k in range(start,end+1):
-                curr_box = aabbs[eidx[k]]
-                if AABBCenter(curr_box)[j] <= curr_split_pos[j]:
-                    lc+=1
-                    AABBGrow(l_aabb, curr_box)
-                else:
-                    rc+=1
-                    AABBGrow(r_aabb, curr_box)
-            cost = (l_aabb.bmax-l_aabb.bmin).sum() * lc + (r_aabb.bmax-r_aabb.bmin).sum()* rc
-            if cost < split_cost:
-                split_cost,split_axis,split_pos = cost,j,curr_split_pos[j]
-                left_count,right_count = lc,rc
+    for i in range(start,end+1):AABBGrow(this_node.box, aabbs[eidx[i]])
+    for j in range(2):
+        if this_node.box.bmax[j]<=this_node.box.bmin[j]:continue
+        stride = (this_node.box.bmax[j] - this_node.box.bmin[j]) / bin_cnt
+        bins = [ [aabb(vec2(FLT_MAX,FLT_MAX) , -vec2(FLT_MAX,FLT_MAX)),0] for _ in range(bin_cnt) ]
+        for i in range(start,end+1):
+            pos = AABBCenter(aabbs[eidx[i]])
+            bin_idx = min(bin_cnt-1, int((pos[j]-this_node.box.bmin[j])/stride))
+            AABBGrow(bins[bin_idx][0],aabbs[eidx[i]])
+            bins[bin_idx][1]+=1
+
+        left_area  = [0.0 for _ in range(bin_cnt-1)]
+        left_cnt   = [0 for _ in range(bin_cnt-1)]
+        left_box   = aabb(vec2(1,1)*FLT_MAX,-vec2(1,1)*FLT_MAX)
+        left_sum   = 0
+        right_area = [0.0 for _ in range(bin_cnt-1)]
+        right_cnt  = [0 for _ in range(bin_cnt-1)]
+        right_box  = aabb(vec2(1,1)*FLT_MAX,-vec2(1,1)*FLT_MAX)
+        right_sum  = 0
+        for i in range(bin_cnt-1):
+            AABBGrow(left_box, bins[i][0])
+            left_area[i] = (left_box.bmax-left_box.bmin).sum()
+            left_sum += bins[i][1]
+            left_cnt[i]  = left_sum
+
+            AABBGrow(right_box, bins[bin_cnt-i-1][0])
+            right_area[bin_cnt-i-2] = (right_box.bmax-right_box.bmin).sum()
+            right_sum += bins[bin_cnt-i-1][1]
+            right_cnt[bin_cnt-i-2]  = right_sum
+        for i in range(bin_cnt-1):
+            cost = left_area[i]*left_cnt[i] + right_area[i]*right_cnt[i]
+            if cost<split_cost: 
+                split_axis,split_pos,split_cost=j,this_node.box.bmin[j]+stride*(i+1),cost
+                left_count,right_count=left_cnt[i],right_cnt[i]
 
     boxs_pts[node_idx][0] = this_node.box.bmin
     boxs_pts[node_idx][2] = vec2(this_node.box.bmin[0],this_node.box.bmax[1])
     boxs_pts[node_idx][4] = this_node.box.bmax
     boxs_pts[node_idx][6] = vec2(this_node.box.bmax[0],this_node.box.bmin[1])
     for j in range(1,8,2):boxs_pts[node_idx][j] = boxs_pts[node_idx][(j+1)%8]
-    
+
     count = end-start+1
     assert(left_count+right_count==count)
 
@@ -86,9 +100,9 @@ def BVHBuildSAH(node_idx,start,end):
 
     this_node.start,this_node.end = INT_MAX,0  # a non-leaf node
     this_node.li     = node_idx+1
-    left_node_cnt    =  BVHBuildSAH(this_node.li, start, start+left_count-1)
+    left_node_cnt    =  BuildBVH(this_node.li, start, start+left_count-1)
     this_node.ri     = this_node.li + left_node_cnt
-    right_node_cnt   =  BVHBuildSAH(this_node.ri, start+left_count,  end)
+    right_node_cnt   =  BuildBVH(this_node.ri, start+left_count,  end)
     assert(left_node_cnt>0 and right_node_cnt>0)
     return left_node_cnt + right_node_cnt + 1
 
@@ -99,7 +113,6 @@ def Initialize():
         p1 = p[2*i] + vec2(ti.sin(ti.random()*10),ti.cos(ti.random()*10)) * (ti.random()*0.2 + 0.05 )
         p[2*i+1] = vec2(min(0.999,max(p1[0],0.001)),min(0.999,max(p1[1],0.001)))
     for i in p: p[i][1]*=0.5
-
     for i in e:
         i0,i1 = 2*i,2*i+1
         e[i] = (i0,i1)
@@ -107,7 +120,9 @@ def Initialize():
         aabbs[i].bmax = vec2(max(p[i0][0],p[i1][0]),max(p[i0][1],p[i1][1]))
     for i in e1:   e1[i] = i
     for i in eidx: eidx[i] = i
-    for i in nodes: nodes[i].start,nodes[i].end = ti.u32(INT_MAX),0
+    for i in nodes:
+        nodes[i].box = aabb(FLT_MAX*vec2(1,1),-FLT_MAX*vec2(1,1)) 
+        nodes[i].start,nodes[i].end = ti.u32(INT_MAX),0
     for i in boxs_idx: boxs_idx[i] = i
 
 def RenderBVH(canvas,depth):
@@ -117,7 +132,7 @@ def RenderBVH(canvas,depth):
         for i,color in s:
             if _==depth: canvas.lines(boxs_pts[i],0.001,boxs_idx,color)
             if nodes[i].start<=nodes[i].end: continue
-            ns.append((nodes[i].li,(231/255,76/255,60/255)))
+            ns.append((nodes[i].li, (231/255,76/255,60/255)))
             ns.append((nodes[i].ri,(52/255,152/255,219/255)))
         s,_ = ns,_+1
 def GetDepthBVH(i):
@@ -126,7 +141,7 @@ def GetDepthBVH(i):
 
 def Main():
     Initialize()
-    BVHBuildSAH(0,0, n-1)
+    BuildBVH(0,0, n-1)
     windows = ti.ui.Window("BVH2D", (1400,1400))
     canvas = windows.get_canvas()
 
@@ -137,7 +152,7 @@ def Main():
         canvas.circles(p, 0.002,(102/255,187/255,106/255))
         RenderBVH(canvas,_)
         _ = (_+1)%d
-        time.sleep(0.5)
+        time.sleep(1)
         windows.show()
 
 Main()
