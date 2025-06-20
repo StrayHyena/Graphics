9a4aef7efcc389eb516565e2a9eec2111e907cd9
PrinceNeZha <384751024@qq.com>
2025-06-20 21:42:18 +0800

    【geo】 add n-directional field

--- Geometry/DifferentialGeometry/MeshOperator.py
+++ Geometry/DifferentialGeometry/MeshOperator.py
@@ -39,8 +39,7 @@ class Halfedge:
         self.fi:    int    = None
         self.boundary = False
 
-    def __str__(self):
-        return f'({self.v0.i},{self.v1.i})'
+    def __str__(self):return f'({self.v0.i},{self.v1.i})'
 
     @property
     def debug_str(self):
@@ -48,16 +47,16 @@ class Halfedge:
                 ' boundary '+str(self.boundary)+ ' fi '+str(self.fi))
 
     @property
-    def vector(self):
-        return self.v1.p - self.v0.p
+    def vector(self):return self.v1.p - self.v0.p
 
     @property
-    def length(self):
-        return np.linalg.norm(self.vector)
+    def length(self):return np.linalg.norm(self.vector)
 
     @property
-    def length2(self):
-        return np.dot(self.vector, self.vector)
+    def length2(self):return np.dot(self.vector, self.vector)
+
+    @property
+    def normalized_vector(self):return self.vector / self.length
 
     @property
     def normal(self):
@@ -74,10 +73,10 @@ class Vertex:
     def __init__(self,i, p):
         self.i = i # index number
         self.p = p # position
+        self.m_area = None
         self.halfedges = []
 
-    def __str__(self):
-        return str(self.i)
+    def __str__(self):return str(self.i)
 
     @property
     def str_halfedges(self):
@@ -86,12 +85,14 @@ class Vertex:
         return ret
 
     @property
-    def he(self):
-        return self.halfedges[0]
+    def he(self):return self.halfedges[0]
 
-    # http://www.cs.cmu.edu/~kmcrane/Projects/Other/TriangleAreasCheatSheet.pdf
-    def CircumcentricDualArea(self):
-        return sum([e.length2*e.cotan+e.prev.length2*e.prev.cotan for e in self.halfedges])/8
+    # CircumcentricDualArea : http://www.cs.cmu.edu/~kmcrane/Projects/Other/TriangleAreasCheatSheet.pdf
+    @property
+    def area(self):
+        if self.m_area is not None: return self.m_area
+        self.m_area = sum([e.length2*e.cotan+e.prev.length2*e.prev.cotan for e in self.halfedges])/8
+        return self.m_area
 
 class Mesh(trimesh.Trimesh):
     def __init__(self, obj_path):
@@ -150,7 +151,7 @@ class Mesh(trimesh.Trimesh):
             h = v.halfedges[0]
             halfedges = []
             while True:
-                h = h.twin.next
+                h = h.prev.twin
                 halfedges.append(h)
                 if h is v.he: break
             v.halfedges = halfedges
@@ -225,7 +226,7 @@ class Mesh(trimesh.Trimesh):
     def x0(self):
         if self.m_x0 is not None:return self.m_x0
         row, col = list(range(self.vn)), list(range(self.vn))
-        data = [v.CircumcentricDualArea() for v in self.Vertexs]
+        data = [v.area() for v in self.Vertexs]
         self.m_x0 = sp.coo_matrix((data,(row,col)),shape=(self.vn,self.vn))
         return self.m_x0
 
@@ -272,7 +273,7 @@ class Mesh(trimesh.Trimesh):
     def _x2(self):
         if self.m__x2 is not None:return self.m__x2
         row, col = list(range(self.vn)), list(range(self.vn))
-        data = [1 / v.CircumcentricDualArea() for v in self.Vertexs]
+        data = [1 / v.area for v in self.Vertexs]
         self.m__x2 = sp.coo_matrix((data, (row, col)), shape=(self.vn, self.vn))
         return self.m__x2
 
@@ -409,6 +410,8 @@ class Mesh(trimesh.Trimesh):
         self.harmonic_bases_on_face = [self.Whitney1Form(base) for base in bases] # for visualize
         return self.m_harmonic_bases
 
+    def BT(self,T):return np.array([np.cross(T[i],self.vertex_normals[i]) for i in range(self.vn)])
+
     # get 1-form on circumcenter of triangle face
     def Whitney1Form(self,oneform):
         face1form = np.array([np.zeros(3) for _ in range(self.fn)])
@@ -449,10 +452,18 @@ class Mesh(trimesh.Trimesh):
 
     def ScalarPoissonProblem(self,idx=0):
         rho = np.array( [(0 if i!=idx else 1) for i in range(self.vn) ])
-        diagM = np.array([v.CircumcentricDualArea() for v in self.Vertexs])
+        diagM = np.array([v.area() for v in self.Vertexs])
         rhs =  rho - np.sum(diagM*rho)/self.area
         u = sp.linalg.spsolve(self.L, np.array(rhs))
         return u
+
+    def VertexTangent(self):
+        # project vertex's halfedge into vertex's tangent plane
+        T = []
+        for v in self.Vertexs:
+            temp = np.cross(v.he.normalized_vector,self.vertex_normals[v.i])
+            T.append(np.cross(self.vertex_normals[v.i],temp))
+        return (np.array([1,0]*self.vn).reshape(self.vn,2) , np.array(T), self.BT(np.array(T)))
 
 # ------------------------------ Some Operation Class  注意下面这些类的成员变量名称最好不要有重复 -----------------------------------------
 class SpectralConformalParameterization(Mesh):
@@ -476,16 +487,15 @@ class SpectralConformalParameterization(Mesh):
         c = np.ones(self.vn)/np.sqrt(self.vn)
 
         # Inverse Power Method
-        x = np.random.rand(self.vn)+1j*np.random.rand(self.vn)
+        x,r = np.random.rand(self.vn)+1j*np.random.rand(self.vn),np.inf
         for _ in range(100):
             x = sp.linalg.spsolve(EC, x)
             x = x - (x.conj().T @ c).conj() * c
             x = x / np.linalg.norm(x)
             l = x.conj().T@EC@x
             r = np.linalg.norm(EC@x-l*x)
-            # print(_,r)
-            if r < 1e-10: break
-        print('WARNING: Spectral Conformal Parameterization may not converge')
+            if r <= 1e-10: break
+        if r>1e-10:print('\033[33mWARNING: Spectral Conformal Parameterization may not converge\033[0m')
         self.uv = x # uv as one complex number
         u, v = np.real(x), np.imag(x)
         u ,v = u - min(u),v - min(v)
@@ -583,8 +593,6 @@ class TrivialConnection(Mesh):
         for _ in range(self.euler_number): k[np.random.choice(self.vn)] += 1
         #注意,beta是dual 2-form
         beta = sp.linalg.spsolve(self.Lc@self._x2,2 * np.pi * k - self.vertex_defects)
-        # generators = self.Generators()
-        # bases = self.HarmonicBases()
         def IntegrateAlongGenerator(oneform,generator):
             ret = 0
             for he in generator:
@@ -645,65 +653,115 @@ class TrivialConnection(Mesh):
         return np.array(face_vector)
 
 # ============= Globally Optimal Direction Fields =============
-class DirectionFields(Mesh):
-    def __init__(self,mesh,n,Senergy):
+class NDirectionField(Mesh):
+    mach = [2.2250738585072014e-308,1.7976931348623157e+308,1.1102230246251565e-16,2.2204460492503131e-16,3.0102999566398120e-01]
+    s11r = [0.0448875760891932036595562553276, 0.0278480909574822965157922173757,0.00394490790249120295818107628687, -0.00157697939158619172562804651751,-0.0000886578217796691901712579357311, 0.0000301708056772263120428135787035,9.521839632337438230089618156e-7, -3.00028307455805582080773625835e-7,-6.14917009583473496433650831019e-9, 1.85133588988085286010092653662e-9,2.67848449041765751590373973224e-11, -7.82394575359355297437491915705e-12,-8.44240072511090922609176843848e-14, 2.41333276776166240844516922196e-14,2.02015531985181413114834031833e-16, -5.68171271075270422851146478874e-17,-3.80082421064644521052871349836e-19, 1.05551739229841670238163200361e-19,5.7758422925275435667221605993e-22, -1.58774695838716531303310462626e-22,-7.24181766014636685673730787292e-25]
+    s11i = [0.100116671557942715638078149123, 0.0429600096728215971268270800599,-0.00799014859477407505770275088389, -0.000664114111384495427035329182866,0.000240714510952202000864758517061, 9.89085259369337382687437812294e-6,-3.22040860178194578481012477174e-6, -8.08401148192350365282200249069e-8,2.48351290049260966544658921605e-8, 4.24154988067028660399867468349e-10,-1.25611378629704490237955971836e-10, -1.56053077919196502557674988724e-12,4.50565044006801278137904597946e-13, 4.2641179237225098728291226479e-15,-1.2084245714879456268965803807e-15, -9.01338537885038989528688031325e-18,2.5180796700698002962991581923e-18, 1.51955263898294940481729370636e-20,-4.19737873024216866691628952458e-21, -2.092488792285595339755624521e-23,5.72708467031136321701747126611e-24]
+    s12r = [ -0.376145877558191778393359413441, 0.0775244431850198578126067647425, 0.0120396593748540634695397747695, -0.00385683684390247509721340352427, -0.000232359275790231209370627606991, 0.0000697318379146209092637310696007, 2.32354473986257272021507575389e-6, -6.71692140309360615694979580992e-7, -1.43946361256617673523038166877e-8, 4.06087820907414336567714443732e-9, 6.10183339004616075548375321861e-11, -1.69196418769523832825063863136e-11, -1.88669746820541798989965091628e-13, 5.16473095452962111184823547686e-14, 4.45066881692009291504139737861e-16, -1.20625107617859803735741992452e-16, -8.28193837331508300767103116139e-19, 2.22680015825230528892642524445e-19, 1.24755889505424049389100515561e-21, -3.33254971913153176741833960484e-22, -1.55307002839777371508497520751e-24]
+    s12i = [0.0527472790869782317601048210983, 0.00823962722148093961886198320927,-0.0205185842051817330153151013327, -0.00184683218270819613487368071941,0.000569681886932212757533488372406, 0.0000248774530818801164177266528608,-7.31121019876580624171992432347e-6, -1.92744564223806538367454388776e-7,5.49794278719049727550379096876e-8, 9.78237385539447442446850072421e-10,-2.7341624177723508216430132999e-10, -3.51839815887772323640101921381e-12,9.68934411607055794052256859665e-13, 9.45703963505047353201918875825e-15,-2.57516976113400217760868402425e-15, -1.97419921753098238455550504742e-17,5.32820017906655555903355375475e-18, 3.29581793797656865402793252539e-20,-8.83137325823594007269279476114e-21, -4.50279718100548728336329365981e-23,1.19941679774924468309434420379e-23]
+    m12r = [0.148523151773238914750879360089, -0.0117856118001224048185631301904,-0.00248887208039014371691400683052, 0.000250045060357076469386198883676,0.0000227217776065076434637230864113, -2.48764935230787745662127026799e-6,-1.32138506847814502856384193414e-7, 1.50966754393693942843767293542e-8,5.3472999553162661403204445045e-10, -6.26136041009708550772228055719e-11,-1.59574066624737000616598104732e-12, 1.89788785691219687197167013023e-13,3.66030609080549274006207730375e-15, -4.39955659500182569051978906011e-16,-6.65848768159000092224193226014e-18, 8.06343127453005031535923212263e-19,9.84397490339224661524630997726e-21, -1.19869887155210161836484730378e-21,-1.20634550494837590549640883469e-23, 1.47512193662595435067359954287e-24,1.24549093756962710863096766634e-26]
+    m12i = [-0.0454399665519585306943416687117, -0.0210517666740874019203591488894,0.00194647501081621201871675259482, 0.000253466068123907163346571754613,-0.0000268083453427538717591876419304, -1.82138740336918117478832696004e-6,2.04357511048425337951376869602e-7, 8.75944656915074206478854298947e-9,-1.01466837126303146739791005703e-9, -3.02573132377805421636557302451e-11,3.57358222114420372764650037191e-12, 7.88121312149152771558608913996e-14,-9.42758576193708862552405242331e-15, -1.60439904050827900099939709069e-16,1.93624791035947590366500765061e-17, 2.62394448214143482490534256935e-19,-3.18700789496399461681365308408e-20, -3.52400207248027768109209530864e-22,4.30074555255053206057921088056e-23, 3.95655079023456015736315286131e-25,-4.84642137915095135859812028886e-26]
+    @staticmethod
+    def inits(series: list[float], n: int, eta: float = mach[2]/10 ) -> int:
+        err,current_n = 0.0,n
+        while err <= eta and current_n > 0:
+            current_n -= 1
+            err += abs(series[current_n])
+        return current_n + 1
+    @staticmethod
+    def csevl(x: float, cs: list[float] ) -> float:
+        b2,b1,b0,twox = 0.0,0,0,2*x
+        for i in range(NDirectionField.inits(cs,len(cs)) - 1, -1, -1):
+            b2,b1 = b1,b0
+            b0 = twox * b1 - b2 + cs[i]
+        return (b0 - b2) / 2.0
+    @staticmethod
+    def s11(t): return NDirectionField.csevl(t, NDirectionField.s11r) + 1j*NDirectionField.csevl(t, NDirectionField.s11i)
+    @staticmethod
+    def s12(t): return NDirectionField.csevl(t, NDirectionField.s12r) + 1j*NDirectionField.csevl(t, NDirectionField.s12i)
+    @staticmethod
+    def m12(t): return NDirectionField.csevl(t, NDirectionField.m12r) + 1j*NDirectionField.csevl(t, NDirectionField.m12i)
+    @staticmethod
+    def DirichletIJDirect(s, gii, gij, gjj):
+        s2,si,is3,s4 = s * s,s*1j,s**3*1j,s**4
+        term1 = (3 * gii + 4 * gij + 3 * gjj) + si * (gii + gij + gjj) - is3 * gij / 6
+        term2 = np.exp(1j*s) * (-(3 * gii + 4 * gij + 3 * gjj) + si * (2 * gii + 3 * gij + 2 * gjj) + s2 * (gii + 2 * gij + gjj) / 2)
+        term3 = (gii - 2 * gij + gjj) / 24 - si * (gii - 2 * gij + gjj) / 60
+        return (term1+term2)/s4 + term3
+    @staticmethod
+    def DirichletII(s, gjj, gjk, gkk):  return 0.25 * ((gjj - 2 * gjk + gkk) + s * s * (gjj + gjk + gkk) / 90)
+    @staticmethod
+    def DirichletIJ(s, gii, gij, gjj):
+        if abs(s) > math.pi:return NDirectionField.DirichletIJDirect(s, gii, gij, gjj)
+        elif s > 0:         return ((gii + gjj) * NDirectionField.s11(s * 2 / math.pi - 1) + gij * NDirectionField.s12(s * 2 / math.pi - 1)).conj()
+        else:               return (gii + gjj) * NDirectionField.s11(-s * 2 / math.pi - 1) + gij * NDirectionField.s12(-s * 2 / math.pi - 1)
+    @staticmethod
+    def MassII():return 1.0 / 6.0
+    @staticmethod
+    def MassIJ(s):
+        if abs(s) > math.pi:return (6*np.exp(1j*s)-6-6j*s+3*s**2+1j*s**3)/3/s**4
+        elif s > 0:         return NDirectionField.m12(s / math.pi * 2 - 1).conj()
+        else:               return NDirectionField.m12(-s / math.pi * 2 - 1)
+
+    def __init__(self,mesh,n=1,Senergy=0):
         self.__dict__ = mesh.__dict__
+        self.n = n
+
         s = np.pi*2/(2*np.pi-self.vertex_defects)
+        # calculate face angle. (Trimesh里的face_angle缺少和点的对应关系)
+        face_angle = {} # key: (face id,vertex id), value: vertex angle in the face
+        face_K = np.zeros(self.fn)
+        for he in self.Halfedges:
+            if he.boundary :continue
+            angle = np.arctan2(2*self.area_faces[he.fi],np.dot(-he.prev.vector,he.vector))
+            face_angle[(he.fi,he.v0.i)] = angle
+            face_K[he.fi] += s[he.v0.i]*angle
+        face_K -= np.pi
         # 默认Vertex的halfedges[0]是basis section(Xi), thetas里存放着每个点对每个incident edge的夹角 (eij与Xi的夹角)
-        # thetas = [[0]]*self.vn
         thetas = {}
         for vi,v in enumerate( self.Vertexs ):
-            for hei,he in enumerate(v.halfedges):
-                if hei==0:
-                    thetas[(vi,he.v1.i)] = 0
-                else:
-                    vj,vj_prev = he.v1.i,v.halfedges[hei-1].v1.i
-                    xi,xj,xj_prev = he.v0.p,he.v1.p,self.vertices[vj_prev]
-                    thetas[(vi,vj)] = thetas[(vi,vj_prev)] + np.arccos(np.dot(xj-xi,xj_prev-xi)/np.linalg.norm(xj-xi)/np.linalg.norm(xj_prev-xi))
+            for hei,he in enumerate(v.halfedges): # 注意，face里的he是和face vertex permutation一致的，这里假设是逆时针 而vertex的he也是逆时针的(see Mesh.__init__,这也是角度的定义)
+                if hei==0:  thetas[(vi,he.v1.i)] = 0
+                else:       thetas[(vi,he.v1.i)] = thetas[(vi,v.halfedges[hei-1].v1.i)] + face_angle[(he.twin.fi,he.v0.i)]
         rho = {}
-        for vi,vj in self.edges:
-            rho[(vi,vj)] = n*(s[vj]*thetas[(vj,vi)]-s[vi]*thetas[(vi,vj)])
-        e_iOmega = [1]*self.fn
+        for vi,vj in self.edges: rho[(vi,vj)] = (s[vj]*thetas[(vj,vi)]-np.pi) -s[vi]*thetas[(vi,vj)] # angle between vj.he and eij (angle between vj.he and eji - pi)  -  angle between vi.he and eij.
+
+        Mbuilder,Abuilder = MatrixBuilder(self.vn,self.vn),MatrixBuilder(self.vn,self.vn)
         for fi,vis in enumerate(self.faces):
-            for i in range(3):
-                e_iOmega[fi]*= np.exp(1j*rho[(vi,vj)])
-        Omega = np.angle(e_iOmega)
-        f1 = lambda s: (3+1j*s+s**4/24-1j*s**5/60+(-3+2j*s+s*s/2)*np.exp(1j*s))/s**4
-        f2 = lambda s: (4+1j*s-1j*s**3/6-s**4/12+1j*s**5/30+(-4+3j*s+s*s)*np.exp(1j*s))/s**4
-        Mbuilder = MatrixBuilder(self.vn,self.vn)
-        Abuilder = MatrixBuilder(self.vn,self.vn)
-        for fi,vis in enumerate(self.faces):
+            om,A = n*face_K[fi],self.area_faces[fi]
+            Mij = A*NDirectionField.MassIJ(om)
             for i in range(3):
                 vi,vj,vk = vis[i],vis[(i+1)%3],vis[(i+2)%3]
-                S,omega,r_jk = self.area_faces[fi],Omega[fi],np.exp(1j*rho[(vj,vk)])
-                M_ii = S/6
-                M_jk = np.conj(r_jk)*S*(6*np.exp(1j*omega)-6-6j*omega+3*omega**2+1j*omega**3)/3/omega**4
+                r_ij = np.exp(1j*n*rho[(vi,vj)])
+                M_ii = A/6
+                M_ij = r_ij.conj()*Mij
+                Mbuilder.AddTriplet(vi,vi,M_ii).AddTriplet(vi,vj,M_ij).AddTriplet(vj,vi,M_ij.conj())
 
                 xi,xj,xk = self.vertices[vi],self.vertices[vj],self.vertices[vk]
-                pjk,pij,pki = xk-xj,xj-xi,xi-xk
-                pjk2,pij2,pki2 = np.dot(pjk,pjk),np.dot(pij,pij),np.dot(pki,pki)
-                pijpik = np.dot(pij,-pki)
-                N_ii = (pjk2+omega**2*(pij2+pijpik+pki2)/90)/S/4
-                N_jk = np.conj(r_jk)*((pij2+pki2)*f1(omega)+pijpik*f2(omega))/S
-
-                A_ii = N_ii - Senergy*omega/S*M_ii
-                A_jk = N_jk - Senergy*(omega/S*M_jk-(1 if (vj,vk) in self.edges_unique else -1)*1j*np.conj(r_jk)/2)
-
-                Mbuilder.AddTriplet(vi,vi,M_ii).AddTriplet(vj,vk,M_jk)
-                Abuilder.AddTriplet(vi,vi,A_ii).AddTriplet(vj,vk,A_jk)
+                pij,pjk,pki = xj-xi,xk-xj,xi-xk
+                N_ii = NDirectionField.DirichletII(om,np.dot(pij,pij),-np.dot(pij,pki),np.dot(pki,pki))/A
+                N_ij = NDirectionField.DirichletIJ(om,np.dot(pki,pki),-np.dot(pki,pjk),np.dot(pjk,pjk))/A
+                A_ii = N_ii - Senergy*om/A*M_ii
+                A_ij = N_ij - Senergy*(om/A*Mij-0.5*1j)*r_ij.conj()
+                Abuilder.AddTriplet(vi,vi,A_ii).AddTriplet(vi,vj,A_ij).AddTriplet(vj,vi,A_ij.conj())
 
         M = Mbuilder.scipy_coo_matrix
         A = Abuilder.scipy_coo_matrix
-        Validator.IsHermitian(M)
-        exit(0)
         x = np.random.rand(self.vn) + 1j * np.random.rand(self.vn)
         for _ in range(100):
             x = sp.linalg.spsolve(A, M@x)
             x = x / np.sqrt(x.conj().T @ M @ x )
             r = np.linalg.norm(A @ x - (x.conj().T@A@x)*(M@x) )
-            print(_,r)
             if r < 1e-10: break
-        return x
+        if r>1e-10:print('\033[33mWARNING: N-Direction-Field may not converge\033[0m')
+        arg = np.arctan2(np.real(x),np.imag(x))/n
+        self.uv = np.column_stack((np.cos(arg),np.sin(arg)))
+        # project vertex's halfedge into vertex's tangent plane
+        T = []
+        for v in self.Vertexs:
+            T.append(np.cross(self.vertex_normals[v.i], np.cross(v.he.normalized_vector, self.vertex_normals[v.i])))
+        self.T = np.array(T)
+        self.BT = self.BT(self.T)
 
 # this class validates several identities or equations numerically
 class Validator:
@@ -725,7 +783,8 @@ class Validator:
     def IsHermitian(M):
         M = M.todense()
         delta = M-np.conj(M.T)
-        print(np.max(np.abs(delta)))
+        if np.allclose(np.max(np.abs(delta)),0):return
+        print('not Hermitian')
 
     # 无论m是否是有边界的(sphere,disk)  ∫∇u•∇vdA+∫v∧*Δu=0
     # 这主要是在于 discrete laplacian-beltrami 在构建的时候对边界情况的处理(cot的一边是0)。
@@ -760,7 +819,8 @@ class Validator:
 # quad-circle 0      |     2
 
 if __name__ == '__main__':
-    # np.set_printoptions(suppress=True,precision=3)
-    # print()
+    np.set_printoptions(suppress=True,precision=3)
+    print()
     # # Validator.Green_1st(Mesh(os.path.join(__file__, '..', 'input', 'quadx.obj')),2)
-    Mesh(os.path.join(__file__, '..', 'input', 'quad-circle.obj'))
+    m = Mesh(os.path.join(__file__, '..', 'input', 'sphere.obj'))
+    NDirectionField(m)
